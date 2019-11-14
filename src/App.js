046e6e3ec4a14e2aff071d990734c183a13d1bdd
Юлия Михайлова <jullidu@gmail.com>
2019-11-14 13:31:20 +0800

    added animated bg (particles)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
+import particlesOptions from './particlesjs-config.json';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App" >
+      <Particles className='particles'
+        params={particlesOptions}
+      />
       <div className='header'>
         <Logo />
         <Navigation />
