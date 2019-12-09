9cc951b22c3ca5a5d88ee49b91bb7b80cc8576cf
Юлия Михайлова <jullidu@gmail.com>
2019-12-09 15:57:21 +0800

    added some frontent changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -44,7 +44,7 @@ class Register extends React.Component {
   render() {
     const { onRouteChange } = this.props;
     return (
-      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-35-l mw6 shadow-5 center">
         <main className="pa4 black-80 tc">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -88,7 +88,7 @@ class Register extends React.Component {
             </fieldset>
             <div>
               <input
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                className="b ph3 pv2 input-reset ba b--black bg-transparent bg-animate hover-bg-black hover-white pointer f6 dib"
                 type="submit"
                 value="Register"
                 onClick={this.onSubmitSignIn}
@@ -97,7 +97,7 @@ class Register extends React.Component {
             <div className="lh-copy mt3">
               <p
                 onClick={() => onRouteChange("signin")}
-                className="f6 link dim black db pointer"
+                className="f5 link dim black db pointer"
               >
                 Sign In
               </p>
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -38,7 +38,7 @@ class Signin extends React.Component {
   render() {
     const { onRouteChange } = this.props;
     return (
-      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-35-l mw6 shadow-5 center">
         <main className="pa4 black-80 tc">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -70,7 +70,7 @@ class Signin extends React.Component {
             </fieldset>
             <div>
               <input
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                className="b ph3 pv2 input-reset ba b--black bg-transparent bg-animate hover-bg-black hover-white pointer f6 dib"
                 type="submit"
                 value="Sign in"
                 onClick={this.onSubmitSignIn}
@@ -79,7 +79,7 @@ class Signin extends React.Component {
             <div className="lh-copy mt3">
               <p
                 onClick={() => onRouteChange("register")}
-                className="f6 link dim black db pointer"
+                className="f5 link dim black db pointer"
               >
                 Register
               </p>
